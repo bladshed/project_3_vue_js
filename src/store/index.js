3bb1f777fc769ab9468f1d917b12e03b59493f74
bladshed <francismarkserrano@yahoo.com>
2022-04-06 00:49:17 +0800

    modified env vars

--- src/store/index.js
+++ src/store/index.js
@@ -7,12 +7,12 @@ import { createStore } from 'vuex';
 const store = createStore({
     state: {
         variables: {
-            VUE_APP_BASE_URL: "https://project-3-hbs.herokuapp.com/",
+            VUE_APP_BASE_URL: "https://loquacious-naiad-713bf7.netlify.app/",
             VUE_APP_BASE_API_URL:"https://project-3-hbs.herokuapp.com/",
             VUE_APP_STRIPE_PUBLISHABLE_KEY:"pk_test_51KgWPJBXapCBJm2WIU6cydkNcrYcoyZWeAy2qJQB0RSCtdre8SDxKe8VfoIWGfIoA51gazsMns9KVa4kxD4GeZ5H00rFt4N0X3",
             VUE_APP_STRIPE_SECRET_KEY:"sk_test_51KgWPJBXapCBJm2WQetZIY5Ps7lZYAYOxjaEvpJxlKTbIJBMsGAwgahO0ZGoQJ0Ts77ZFIlCjShBgt9VbvVLJnBP005QlpPxnw",
-            VUE_APP_STRIPE_SUCCESS_URL:"http://localhost:8080/checkout/success",
-            VUE_APP_STRIPE_CANCEL_URL:"http://localhost:8080/cart"
+            VUE_APP_STRIPE_SUCCESS_URL:"https://loquacious-naiad-713bf7.netlify.app/checkout/success",
+            VUE_APP_STRIPE_CANCEL_URL:"https://loquacious-naiad-713bf7.netlify.app/cart"
         }
     },
     getters: {
